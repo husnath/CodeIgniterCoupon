f0a8378d5a09a50ce4571bebf833575338271192
husnath p <husnathpalakkal773@gmail.com>
2021-12-31 12:59:45 +0530

    second commit

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,6 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
+
+
+
 	/**
 	 * Index Page for this controller.
 	 *
@@ -23,6 +26,7 @@ class Welcome extends CI_Controller {
 
 	 
 	public function __construct() {
+		
 		parent::__construct();
 		$this->load->model('coupon_model');
 
